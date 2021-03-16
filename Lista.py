dc54cf985690c6207021077cad2dfa0ce688b18c
RHenrique22 <72585927+RHenrique22@users.noreply.github.com>
2021-03-15 23:39:52 -0300

    Etapa 1

---
+++ Lista.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, dado = None):
+        self._dado = dado
+        self._prox = None
+    
+    @property
+    def dado(self):
+        return self._dado
+    
+    @dado.setter
+    def dado(self, novo_dado):
+        self._dado = novo_dado
+    
+    @property
+    def proximo(self):
+        return self._prox
+    
+    @proximo.setter
+    def proximo(self, novo):
+        self._prox = novo
+
+
+class ListaEncadeada:
+    def __init__(self):
+        self._inicio = None
+        self._tamanho = 0
